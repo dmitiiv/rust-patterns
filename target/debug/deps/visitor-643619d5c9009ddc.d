ad67fd8f50bb14d7e38cd82365ec645072bc7140
dm <davarlamov810@gmail.com>
2024-10-28 20:35:57 +0300

    feat: add visitor template

---
+++ target/debug/deps/visitor-643619d5c9009ddc.d
@@ -0,0 +1,5 @@
+/home/dm/User/homework/rust-patterns/target/debug/deps/libvisitor-643619d5c9009ddc.rmeta: visitor/src/main.rs
+
+/home/dm/User/homework/rust-patterns/target/debug/deps/visitor-643619d5c9009ddc.d: visitor/src/main.rs
+
+visitor/src/main.rs:
