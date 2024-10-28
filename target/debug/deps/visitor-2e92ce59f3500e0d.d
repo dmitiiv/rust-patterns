924dada86bab89aa9ae1f97fd6699b7f9612b174
dm <davarlamov810@gmail.com>
2024-10-28 20:41:49 +0300

    feat: add visitor template

--- target/debug/deps/visitor-2e92ce59f3500e0d.d
+++
@@ -1,5 +0,0 @@
-/home/dm/User/homework/rust-patterns/target/debug/deps/libvisitor-2e92ce59f3500e0d.rmeta: visitor/src/main.rs
-
-/home/dm/User/homework/rust-patterns/target/debug/deps/visitor-2e92ce59f3500e0d.d: visitor/src/main.rs
-
-visitor/src/main.rs:
--- target/debug/deps/visitor-643619d5c9009ddc.d
+++
@@ -1,5 +0,0 @@
-/home/dm/User/homework/rust-patterns/target/debug/deps/libvisitor-643619d5c9009ddc.rmeta: visitor/src/main.rs
-
-/home/dm/User/homework/rust-patterns/target/debug/deps/visitor-643619d5c9009ddc.d: visitor/src/main.rs
-
-visitor/src/main.rs:
