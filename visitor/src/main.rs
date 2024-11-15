2c987b5d07b66e10a007b02748acf4ae4b3cf10a
davarlamov <davarlamov@avito.ru>
2024-11-15 14:07:57 +0300

    feat: add func aproach

--- visitor/src/main.rs
+++ visitor/src/main.rs
@@ -1,46 +1,10 @@
+mod func;
+mod oop;
+
+use func::func;
+use oop::oop;
 fn main() {
-    let visitor = ConcreteVisitor {};
-
-    let list: Vec<Box<dyn Element>> = vec![Box::new(ElementA {}), Box::new(ElementB {})];
-
-    for mut elem in list {
-        elem.accept(&visitor);
-    }
+    oop();
+    func();
     println!("Hello, world!");
-}
-
-trait Visitor {
-    fn visit_element_a(&self, element: &mut ElementA);
-    fn visit_element_b(&self, element: &mut ElementB);
-}
-
-struct ConcreteVisitor {}
-
-impl Visitor for ConcreteVisitor {
-    fn visit_element_a(&self, element: &mut ElementA) {
-        todo!()
-    }
-
-    fn visit_element_b(&self, element: &mut ElementB) {
-        todo!()
-    }
-}
-
-trait Element {
-    fn accept(&mut self, visitor: &dyn Visitor);
-}
-
-struct ElementA {}
-
-impl Element for ElementA {
-    fn accept(&mut self, visitor: &dyn Visitor) {
-        visitor.visit_element_a(self);
-    }
-}
-struct ElementB {}
-
-impl Element for ElementB {
-    fn accept(&mut self, visitor: &dyn Visitor) {
-        visitor.visit_element_b(self);
-    }
 }
